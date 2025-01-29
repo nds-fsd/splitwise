bbd319372060049099c45572436454ef1ca1548f
Jorge <jorgealvarezfrancino@gmail.com>
2025-01-29 20:31:02 +0100

    SPL-23-Add-Expense

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,25 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-// import User from './pages/user/user';
-import UserExpenses from "./pages/user/userExpenses/userExpenses";
-import GroupExpenses from "./pages/group/groupExpenses/groupExpenses";
-import GroupDetailsPage from "./components/GroupDetails";
 import Layout from "./components/Layout/Layout";
+import Groups from "./pages/groups/groups";
+import GroupExpenses from "./pages/groups/expense/expense";
+import UserExpenses from "./pages/user/userExpenses/userExpenses";
+import NoMatch from "./pages/noMatch/noMatch"
 import { QueryClient, QueryClientProvider } from "react-query";
-import CreateGroupModal from "./components/CreateGroupModal";
-import React, { useState } from "react";
-import User from "./components/pages/user"; // Asegúrate de que la ruta sea correcta
-import Userlist from "./components/pages/userList";
-import RegisterForm from "./components/pages/react-hook-form/reactHookForm";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [groups, setGroups] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const createGroup = (newGroup) => {
-    setGroups([...groups, newGroup]);
-  };
 
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout forceUpdate={() => setForceUpdate(!forceUpdate)} />
-            }
-          >
+          <Route path="/" element={<Layout forceUpdate={() => setForceUpdate(!forceUpdate)} />}>
             <Route path="/user/expenses" element={<UserExpenses />} />
-            <Route path="/group/expenses" element={<GroupExpenses />} />
-            {/* Ruta para ver un usuario con un ID dinámico */}
-            <Route path="/users/:id" element={<User />} />
-            {/* Ruta de ejemplo para la página principal */}
-            <Route path="/" element={<div>Bienvenido a la app</div>} />
-            <Route path="/register" element={<RegisterForm />} />
-            <Route path="/users" element={<Userlist />} />
-            <Route path="/" element={<Navigate to="/group-details" />} />
-            <Route
-              path="/group-details"
-              element={<GroupDetailsPage groups={groups} />}
-            />
+            <Route path="/groups" element={<Groups />} />
+            <Route path="/groups/:groupId/expenses" element={<GroupExpenses />} />
+            <Route path='*' element={<NoMatch />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,11 +1,24 @@
 import { Link, Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import styles from './Layout.module.css';
+import { Slide, ToastContainer } from 'react-toastify';
 const Layout = () => {
     return (
-        <div>
+        <div className={styles.app}>
             <Header />
-            <main>
+            <main className={styles.main}>
                 <Outlet />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    closeOnClick={false}
+                    pauseOnHover={true}
+                    draggable
+                    progress={undefined}
+                    theme="dark"
+                    transition={Slide}
+                />
             </main>
         </div>
     );

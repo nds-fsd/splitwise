059010092558a16d800d3d45b6d2372274d62d8e
Jorge <jorgealvarezfrancino@gmail.com>
2025-02-15 16:42:41 +0100

    Added dark mode context provider to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import User from "./pages/user/userProfile/user"; // Asegúrate de que la ruta s
 import Userlist from "./pages/user/userList/userList";
 import RegisterForm from "./components/register/registerForm";
 import Login from "./components/login/loginForm";
+import { DarkModeContextProvider } from "./context/darkModeContext";
 
 const queryClient = new QueryClient();
 
@@ -19,23 +20,25 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout forceUpdate={() => setForceUpdate(!forceUpdate)} />}>
-            <Route path="/user/expenses" element={<UserExpenses />} />
-            <Route path="/login" element={<Login forceUpdate={() => setForceUpdate(!forceUpdate)} />} />
-            {/* Ruta para ver un usuario con un ID dinámico */}
-            <Route path="/users/:id" element={<User />} />
-            {/* Ruta de ejemplo para la página principal */}
-            <Route path="/register" element={<RegisterForm />} />
-            <Route path="/users" element={<Userlist />} />
-            <Route path="/groups" element={<Groups />} />
-            <Route path="/groups/:groupId/expenses" element={<GroupExpenses />} />
-            <Route path='*' element={<NoMatch />} />
+      <DarkModeContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout forceUpdate={() => setForceUpdate(!forceUpdate)} />}>
+              <Route path="/user/expenses" element={<UserExpenses />} />
+              <Route path="/login" element={<Login forceUpdate={() => setForceUpdate(!forceUpdate)} />} />
+              {/* Ruta para ver un usuario con un ID dinámico */}
+              <Route path="/users/:id" element={<User />} />
+              {/* Ruta de ejemplo para la página principal */}
+              <Route path="/register" element={<RegisterForm />} />
+              <Route path="/users" element={<Userlist />} />
+              <Route path="/groups" element={<Groups />} />
+              <Route path="/groups/:groupId/expenses" element={<GroupExpenses />} />
+              <Route path='*' element={<NoMatch />} />
 
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </DarkModeContextProvider>
     </QueryClientProvider>
   );
 }

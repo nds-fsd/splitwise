d493989c80f4a8f5ad4d77a8ad10a4a65080f40b
Jorge <jorgealvarezfrancino@gmail.com>
2025-02-17 14:32:24 +0100

    Added dark mode styles for layout and modal components

--- frontend/src/components/modal/modal.jsx
+++ frontend/src/components/modal/modal.jsx
@@ -1,9 +1,12 @@
+import { useDarkMode } from '../../context/darkModeContext';
 import styles from './modal.module.css';
 
 const Modal = ({ children }) => {
+    const { darkMode } = useDarkMode();
+
     return (
         <div className={styles.modal}>
-            <div className={styles.content}>
+            <div className={`${styles.content} ${darkMode ? styles.contentDark : ''}`}>
                 {children}
             </div>
         </div>
